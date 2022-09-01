8fd082aeed4bccecbac2ce2a59b042d3f5acbcb6
EducationManMaster <71923384+EducationManMaster@users.noreply.github.com>
2022-09-01 10:31:22 +0200

    Add files via upload

--- gui/main.js
+++ gui/main.js
@@ -44,11 +44,18 @@ function setInfo(state) {
 // ---------------------------------------------------------------------------
 //
 
-function testWarnung(wert) {
+function test(element, wert) {
 
-  var energie = document.getElementById("energie");
-  var value   = energie.getElementsByClassName("tile_value")[0];
-  var state   = energie.getElementsByClassName("tile_footer")[0];
+  var elm = document.getElementById(element.toLowerCase());
+  var value = elm.getElementsByClassName("tile_value")[0];
+  var state = elm.getElementsByClassName("tile_footer")[0];
+
+  if(wert <= 4000){ testWarn(value, state, wert);
+  }else{            testBad(value, state, wert); }
+
+}
+
+function testWarn(value, state, wert) {
 
   value.innerHTML = wert;
   state.className = "tile_footer tile_footer_warn";
@@ -57,11 +64,7 @@ function testWarnung(wert) {
 
 }
 
-function testProblem(wert) {
-
-  var energie = document.getElementById("energie");
-  var value   = energie.getElementsByClassName("tile_value")[0];
-  var state   = energie.getElementsByClassName("tile_footer")[0];
+function testBad(value, state, wert) {
 
   value.innerHTML = wert;
   state.className = "tile_footer tile_footer_bad";
