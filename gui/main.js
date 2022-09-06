1191d4fb37601ccdcabb654c7b6ed9dbe000fc6b
EducationManMaster <71923384+EducationManMaster@users.noreply.github.com>
2022-09-06 11:51:25 +0200

    Add files via upload

--- gui/main.js
+++ gui/main.js
@@ -180,3 +180,152 @@ document.addEventListener("DOMContentLoaded", function() {
   applyGUI();
 
 });
+
+
+
+
+
+
+
+
+
+
+var user_settings = {
+
+  people:1,
+  warnings:true,
+  problems:true
+
+};
+
+var user_values = {
+
+  power :{ avg:110,  warn:null, prob:null },
+  water :{ avg:4000, warn:null, prob:null },
+  gas   :{ avg:500,  warn:null, prob:null },
+  carbon:{ avg:100,  warn:null, prob:null },
+
+  warn:0.65,
+  prob:1.20,
+
+  calc:function() {
+
+    user_values.power.warn  = Math.round((user_values.power.avg * user_settings.people) * user_values.warn);
+    user_values.power.prob  = Math.round((user_values.power.avg * user_settings.people) * user_values.prob);
+
+    user_values.water.warn  = Math.round((user_values.water.avg * user_settings.people) * user_values.warn);
+    user_values.water.prob  = Math.round((user_values.water.avg * user_settings.people) * user_values.prob);
+
+    user_values.gas.warn    = Math.round((user_values.gas.avg * user_settings.people) * user_values.warn);
+    user_values.gas.prob    = Math.round((user_values.gas.avg * user_settings.people) * user_values.prob);
+
+    user_values.carbon.warn = Math.round((user_values.carbon.avg * user_settings.people) * user_values.warn);
+    user_values.carbon.prob = Math.round((user_values.carbon.avg * user_settings.people) * user_values.prob);
+
+  }
+
+};
+
+
+
+
+var testData = [2, 3, 2, 4, 3, 5, 6, 1, 7, 3, 2, 4, 5, 2, 3, 2, 3, 5, 1, 4, 2];
+
+
+function drawDaily(type = "power") {
+
+  var canvas;
+  var data;
+
+  switch (type) {
+
+    case "power":
+      canvas = "canvas_day_power";
+      data   = testData;
+    break;
+
+  }
+
+  var l = (data.length - 7);
+  if(l < 0){ l = 0; }
+
+  var max = 0;
+  for (var i = 0; i < l; i++) { if(data[i] > max){ max = data[i]; }}
+
+  for (var i = 0; i < l; i++) {
+
+    var w = c.width / l;
+    //var h = 
+
+
+  }
+
+}
+
+
+function drawMonthly(type = "power") {
+
+  user_values.calc();
+
+  var canvas;
+  var warn;
+  var prob;
+  var data;
+
+  switch (type) {
+
+    case "power":
+      canvas = "canvas_power";
+      warn   = user_values.power.warn;
+      prob   = user_values.power.prob;
+      data   = testData;
+    break;
+
+  }
+
+  var c = document.getElementById(canvas);
+
+  c.width  = c.offsetWidth*2
+  c.height = c.offsetHeight*2
+
+  var ctx = c.getContext("2d");
+
+  var w = (c.width  * 0.75) / data.length;
+  var h = (c.height * 0.75) / 100;
+
+  var dx = 0;
+  var dy = c.height;
+
+  var sum   = 0;
+  var total = 0;
+
+  for (var i = 0; i < data.length; i++) { total += data[i]; }
+  for (var i = 0; i < data.length; i++) {
+
+    sum += data[i];
+
+    var x = (w * i);
+    var y = (c.height - (h * ((100 / total) * sum)));
+
+    ctx.strokeStyle = "#9CCC65";
+    ctx.lineWidth = 4;
+    ctx.lineCap = "round";
+    ctx.moveTo(dx, dy);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    dx = x;
+    dy = y;
+
+  }
+
+  ctx.setLineDash([10, 15]);
+  ctx.moveTo(0, dy);
+  ctx.lineTo(c.width, y);
+  ctx.stroke();
+
+  ctx.font = "30px Arial";
+  ctx.fillStyle = "#9CCC65";
+  ctx.fillText((sum + " kW/h"), dx, dy+35);
+
+}
